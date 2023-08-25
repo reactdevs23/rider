49ec6aaf099fd971d6ecb7f4d7417b98a48617c6
dalimkumar604 <dalimkumar212582@gmail.com>
2023-08-25 22:36:42 +0600

    loginPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
+import Signin from "./pages/SignIn/SignIn";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const hideHeader = location.pathname === "/signin";
+  useEffect(() => {
+    navigate("/signin");
+  }, []);
+
   return (
     <>
-      <Header />
+      {!hideHeader && <Header />}
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
       </Routes>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
+
 import RadientOnes from "../components/Home/RadientOnes/RadientOnes";
 import Radients from "../components/Home/Radients/Radients";
 
 const Home = () => {
   return (
-    <div className="mainContainer ">
-      <div className="mainWrapper">
-        {" "}
-        <RadientOnes />
-        <Radients />
+    <>
+      <div className="mainContainer ">
+        <div className="mainWrapper">
+          {" "}
+          <RadientOnes />
+          <Radients />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
